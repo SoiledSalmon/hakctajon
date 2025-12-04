88edbb54afffdf6d95caee91ff25fe08a4fcc2f2
Ronith <ronith.xa@gmail.com>
2025-12-05 02:40:26 +0530

    integrating firebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,13 @@ import 'package:ai_loan_buddy/theme/app_theme.dart';
 import 'package:ai_loan_buddy/utils/navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const ProviderScope(child: AILoanBuddyApp()));
 }
 
